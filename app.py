ae127fcf69ba01f1e45546e98d398f9cc7c3258b
keng <114017716+sompott@users.noreply.github.com>
2025-04-27 13:16:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def download_file():
     return send_from_directory(directory="static/file", path="Resume.png", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
